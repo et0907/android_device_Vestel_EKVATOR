9fc75d2a9bb33a7e9d20541c6a69f6a84cf714b0
et0907 <erento.0709@gmail.com>
2026-09-10 03:06:38 +0300

    Update omni_EKVATOR.mk

--- omni_EKVATOR.mk
+++ omni_EKVATOR.mk
@@ -9,8 +9,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Inherit from EKVATOR device
 $(call inherit-product, device/vestel/EKVATOR/device.mk)
